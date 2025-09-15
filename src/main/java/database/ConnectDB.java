cc84ac235feacd5ef0983ba591eb87e4a1660a3d
Thien <ledinhthienpm@gmail.com>
2025-09-15 17:25:06 +0700

    connect database

--- src/main/java/database/ConnectDB.java
+++ src/main/java/database/ConnectDB.java
@@ -1,5 +1,34 @@
 package database;
 
-public class ConnectDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectDB {
+    public static Connection connect() {
+        String url = "jdbc:mysql://localhost:3306/library_management";
+        String user = "root";
+        String password = "123456";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Database connect successfully!");
+            return conn;
+        } catch (SQLException e) {
+            // TODO: handle exception
+            System.out.println("Lỗi kết nối: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void disconnect(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+                System.out.println("Dong ket noi thanh cong");
+            }
+        } catch (Exception e) {
+            System.out.println("Lỗi kết nối: " + e.getMessage());
+        }
+    }
 }
